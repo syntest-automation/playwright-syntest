e1bd3ebf05a0a3936465615fcb10608e71f7e92a
BharadwajShivam <Shivam@propertyguru.tech>
2024-09-09 12:24:26 +0530

    test: added lighthouse support

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,21 @@
-import pluginJs from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
   {
-    files: ['**/*.{js,ts}']
+    files: ["**/*.{ts}"],
   },
   {
     languageOptions: {
-      globals: globals.browser
-    }
+      globals: globals.browser,
+    },
   },
-  pluginJs.configs.recommended,
+  pluginTs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
-      'no-unused-vars': 'error',
-      'no-undef': 'error'
-    }
-  }
-]
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+];
